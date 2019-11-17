c00a35a931f808da0809e31eee4285962535e307
Slav Savov <vokalin_one@yahoo.com>
2019-11-17 16:23:34 +0200

    Modified app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,26 @@
 <?php
+
 session_start();
 require '../vendor/autoload.php';
 require '../app/config/config.php';
 
-$app = new Slim\Slim([
-    'templates.path' => __DIR__ . '/../resources/views',
-    'view' => new \Slim\Views\Twig(__DIR__ . '/../resources/views'),
-        ]);
+$app = new \Slim\Slim(array(
+    'templates.path' => '/var/www/html/blog/resources/views/',
+    'view' => new Slim\Views\Twig('/var/www/html/blog/resources/views/')
+));
+$view = $app->view();
 
-$app->container->singleton('database', function () use ($db){
+$view->parserOptions = array(
+    'debug' => true,
+    'cache' => false
+);
+$view->parserDirectory = 'Twig';
+
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+);
+
+$app->container->singleton('database', function () use ($db) {
     $dbConn = new \Db\Connections\MySqlConnection((object) $db['mysql']);
     return $dbConn->getConnection();
 });
